bf27d1ca3a5f48d27b8232aee06e17d958a8a725
Sarthak Sen <81672180+Sarthak-Sen@users.noreply.github.com>
2021-11-14 16:58:37 +0530

    signup/background

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,27 @@
 import React from "react";
+import bgImg from "../images/background.jpg";
+import "../styles/front.css";
+import { Container } from "react-bootstrap";
+import SignUp from "./SignUp";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hi- Maaz and Amrit</h1>
-      <h2>Regards- Sarthak</h2>
+    <div
+      style={{
+        backgroundImage: `url(${bgImg})`,
+        width: "100%",
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+      }}
+    >
+      <Container
+        className="d-flex align-items-center justify-content-center"
+        style={{ minHeight: "100vh" }}
+      >
+        <div className="w-100" style={{ maxWidth: "400px" }}>
+          <SignUp />
+        </div>
+      </Container>
     </div>
   );
 }
